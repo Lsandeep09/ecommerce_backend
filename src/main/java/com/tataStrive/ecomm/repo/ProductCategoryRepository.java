e99c7a1bf692f19c1862c6d524a7cb7e0b09a598
Lsandeep09 <lsandeepr50@gmail.com>
2025-12-29 23:22:06 +0530

    parital commit for checking the db is working

--- src/main/java/com/tataStrive/ecomm/repo/ProductCategoryRepository.java
+++ src/main/java/com/tataStrive/ecomm/repo/ProductCategoryRepository.java
@@ -1,10 +1,11 @@
 package com.tataStrive.ecomm.repo;
 
 import com.tataStrive.ecomm.entities.Product;
+import com.tataStrive.ecomm.entities.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 
-public interface ProductCategoryRepository extends JpaRepository<Product,Long> {
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
--- src/main/java/com/tataStrive/ecomm/repo/ProductRepository.java
+++ src/main/java/com/tataStrive/ecomm/repo/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.tataStrive.ecomm.repo;
 
+import com.tataStrive.ecomm.entities.Product;
 import com.tataStrive.ecomm.entities.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<ProductCategory,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
