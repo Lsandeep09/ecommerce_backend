e8b0af205334063cc40c327a2f1a72155cc891c6
Lsandeep09 <lsandeepr50@gmail.com>
2025-12-30 23:15:24 +0530

    partially backend completed

--- src/main/java/com/tataStrive/ecomm/repo/ProductCategoryRepository.java
+++ src/main/java/com/tataStrive/ecomm/repo/ProductCategoryRepository.java
@@ -5,7 +5,7 @@ import com.tataStrive.ecomm.entities.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category",exported = true)
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
