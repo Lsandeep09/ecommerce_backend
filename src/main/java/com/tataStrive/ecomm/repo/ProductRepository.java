96848206491bc053dc93805d6244d6ed88e7db15
Lsandeep09 <lsandeepr50@gmail.com>
2026-01-02 15:13:18 +0530

    unused import

--- src/main/java/com/tataStrive/ecomm/repo/ProductRepository.java
+++ src/main/java/com/tataStrive/ecomm/repo/ProductRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.*;
+
 
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends JpaRepository<Product,Long> {
